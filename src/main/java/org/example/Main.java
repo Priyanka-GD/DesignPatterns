8cd21c5de23977d00b081ee8d61eda2ddcf9785c
“PriyankaGD” <priyanka.dastidar@gmail.com>
2024-09-06 10:39:27 -0700

    Singleton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class Main {
+    //JVM searches for main method- treats it as a point of execution
+    //Method doesn't need to be instantiated, so static
+    //Args is for command line arguments to pass
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/main/java/org/example/creationalpattern/Singleton.java
@@ -0,0 +1,32 @@
+package org.example.creationalpattern;
+
+public class Singleton {
+    private static volatile Singleton instance;
+    private String data;
+
+    //Only access using public static main method
+    private Singleton(String data) {
+        this.data = data;
+    }
+
+    public static Singleton getInstance(String data) {
+        //Thread safe -- Only one thread access at a time
+        //Limiting synchronization of constructing a new instance of this field
+        //Skipping Synchronization when retrieving an already created instance
+        //Everytime we try to access this variable instance, we need to read it directly from main memory, so to, access it twice
+        //Avoid that we create local instance and store it there, method overall performance by 40%
+        Singleton result = instance;
+        if (instance == null) { // - A
+            synchronized (Singleton.class) {// - A
+                if (result == null)// - A
+                    //A shared variable stored in memory can reference partially constructed object other threads consider it as initialized and use it
+                    //causing application to crash , so  singleton instance is made volatile
+                    result = new Singleton(data);
+            }
+        }
+        return result;
+    }
+    //Not Robust enough if synchronized not there
+    //Double check locking
+
+}

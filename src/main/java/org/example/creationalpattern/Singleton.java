e6ce0130d2e2d3ef0faeb395fe367b8fdff3fa3e
“PriyankaGD” <priyanka.dastidar@gmail.com>
2024-09-06 10:41:23 -0700

    comments

--- src/main/java/org/example/creationalpattern/Singleton.java
+++ src/main/java/org/example/creationalpattern/Singleton.java
@@ -16,9 +16,9 @@ public class Singleton {
         //Everytime we try to access this variable instance, we need to read it directly from main memory, so to, access it twice
         //Avoid that we create local instance and store it there, method overall performance by 40%
         Singleton result = instance;
-        if (instance == null) { // - A
-            synchronized (Singleton.class) {// - A
-                if (result == null)// - A
+        if (instance == null) { 
+            synchronized (Singleton.class) {
+                if (result == null)
                     //A shared variable stored in memory can reference partially constructed object other threads consider it as initialized and use it
                     //causing application to crash , so  singleton instance is made volatile
                     result = new Singleton(data);
@@ -28,5 +28,8 @@ public class Singleton {
     }
     //Not Robust enough if synchronized not there
     //Double check locking
+    //Singleton disables all means of creating objects of a class except for special static creation method.
+    //returns existing instance if it has been already created
+    //Should be used when class has single instance available
 
 }
